fbd848b3bcbd28e3f04836911e0a3a4f4f799cd7
StanleyGemstone <stanleynduchi@gmail.com>
2023-02-23 23:54:22 +0100

    just2

--- shell.h
+++ shell.h
@@ -83,7 +83,7 @@ typedef struct passinfo
 	int readfd;
 	int histcount;
 } info_t;
-#define INFO_INIT \
+#define INFO_INIT
 {NULL, NULL, NULL, 0, 0, 0, 0, NULL, NULL, NULL, NULL, NULL, 0, 0, NULL, \ 0, 0, 0}
 /**
  * struct builtin - contains a builtin string and related function
